37ffdd94ee74c4cc0e984a930911549169cbf88b
Mrzakirullahkhan <mrzak802@gmail.com>
2023-03-26 22:13:52 +0500

    Analog clock

---
+++ app.js
@@ -0,0 +1,22 @@
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('seconds');
+
+
+
+function timeDisplay(){
+    let date = new Date();
+
+    let hh  = date.getHours();
+    let mm  = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+    
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+    second.style.transform = `rotate(${sRotation}deg)`;
+}
+setInterval(timeDisplay, 1000);
